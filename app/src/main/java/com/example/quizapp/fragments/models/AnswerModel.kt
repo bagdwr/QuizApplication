a1fb7ab9b6a6cb77a11a6aff386d4e1c197271df
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2021-07-06 10:49:11 +0600

    pass answers from childFragment to parentFragment

--- app/src/main/java/com/example/quizapp/fragments/models/AnswerModel.kt
+++ app/src/main/java/com/example/quizapp/fragments/models/AnswerModel.kt
@@ -4,11 +4,36 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 class AnswerModel(
-    @SerializedName("answer_a") val answer_a:String,
-    @SerializedName("answer_b") val answer_b:String,
-    @SerializedName("answer_c") val answer_c:String,
-    @SerializedName("answer_d") val answer_d:String,
-    @SerializedName("answer_e") val answer_e:String,
-    @SerializedName("answer_f") val answer_f:String
+    @SerializedName("answer_a") val answer_a:String?,
+    @SerializedName("answer_b") val answer_b:String?,
+    @SerializedName("answer_c") val answer_c:String?,
+    @SerializedName("answer_d") val answer_d:String?,
+    @SerializedName("answer_e") val answer_e:String?,
+    @SerializedName("answer_f") val answer_f:String?
 
-):Serializable
+):Serializable {
+
+
+    fun getAnswers(): List<String> {
+        val result = mutableListOf<String>()
+        if(answer_a != null) {
+            result.add(answer_a)
+        }
+        if(answer_b != null) {
+            result.add(answer_b)
+        }
+        if(answer_c != null) {
+            result.add(answer_c)
+        }
+        if(answer_d != null) {
+            result.add(answer_d)
+        }
+        if(answer_e != null) {
+            result.add(answer_e)
+        }
+        if(answer_f != null) {
+            result.add(answer_f)
+        }
+        return result
+    }
+}
--- app/src/main/java/com/example/quizapp/fragments/question/OneAnswerFragment.kt
+++ app/src/main/java/com/example/quizapp/fragments/question/OneAnswerFragment.kt
@@ -12,51 +12,63 @@ import androidx.fragment.app.viewModels
 import com.example.quizapp.R
 import com.example.quizapp.fragments.models.QuestionModel
 
-class OneAnswerFragment:Fragment() {
+class OneAnswerFragment : Fragment() {
     private val viewModel: QuestionViewModel by viewModels()
     private lateinit var radioGroup: RadioGroup
-    private lateinit var radio1:RadioButton
-    private lateinit var radio2:RadioButton
-    private lateinit var radio3:RadioButton
-    private lateinit var radio4:RadioButton
-    private lateinit var radio5:RadioButton
-    private lateinit var radio6:RadioButton
-    companion object{
-         private const val answers="ANSWERS"
-         fun createFragment(questionModel: QuestionModel):OneAnswerFragment{
-              val fragment=OneAnswerFragment()
-              val bundle=Bundle()
-              bundle.putSerializable(answers,questionModel)
-              fragment.arguments=bundle
-              return fragment
-         }
+    private lateinit var radio1: RadioButton
+    private lateinit var radio2: RadioButton
+    private lateinit var radio3: RadioButton
+    private lateinit var radio4: RadioButton
+    private lateinit var radio5: RadioButton
+    private lateinit var radio6: RadioButton
+
+    companion object {
+        private const val answers = "ANSWERS"
+        fun createFragment(questionModel: QuestionModel): OneAnswerFragment {
+            val fragment = OneAnswerFragment()
+            val bundle = Bundle()
+            bundle.putSerializable(answers, questionModel)
+            fragment.arguments = bundle
+            return fragment
+        }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view=inflater.inflate(R.layout.one_answer_fragment,container,false)
-        radioGroup=view.findViewById(R.id.ragioGroup)
-        radio1=view.findViewById(R.id.radio1)
-        radio2=view.findViewById(R.id.radio2)
-        radio3=view.findViewById(R.id.radio3)
-        radio4=view.findViewById(R.id.radio4)
-        radio5=view.findViewById(R.id.radio5)
-        radio6=view.findViewById(R.id.radio6)
+        val view = inflater.inflate(R.layout.one_answer_fragment, container, false)
+        radioGroup = view.findViewById(R.id.ragioGroup)
+        radio1 = view.findViewById(R.id.radio1)
+        radio2 = view.findViewById(R.id.radio2)
+        radio3 = view.findViewById(R.id.radio3)
+        radio4 = view.findViewById(R.id.radio4)
+        radio5 = view.findViewById(R.id.radio5)
+        radio6 = view.findViewById(R.id.radio6)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (arguments!=null){
-            val questionModel=arguments?.getSerializable(answers) as QuestionModel
-            radio1.text=questionModel.answerModel.answer_a.toString()
-            radio2.text=questionModel.answerModel.answer_b.toString()
-            radio3.text=questionModel.answerModel.answer_c.toString()
-            radio4.text=questionModel.answerModel.answer_d.toString()
-//            radio5.text=questionModel.answerModel.answer_e.toString()
-//            radio6.text=questionModel.answerModel.answer_f.toString()
+        val listOfRadioBtn: List<RadioButton> =
+            listOf(
+                radio1, radio2, radio3, radio4, radio5, radio6
+            )
+        if (arguments != null) {
+            val questionModel = arguments?.getSerializable(answers) as QuestionModel
+            val answers = questionModel.answerModel.getAnswers()
+            for (i in answers.indices) {
+                listOfRadioBtn[i].text = answers[i]
+            }
+        }
+
+        listOfRadioBtn.forEach { radioButton ->
+            radioButton.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    (parentFragment as QuestionFragment).onAnswerClicked(radioButton.text.toString())
+                }
+            }
         }
     }
 //    fun checkButton(view: View){
